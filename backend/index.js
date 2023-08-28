e7fc3184836871b5bbc7e93c383bcf06281c68ff
KingKyoTrue <tanadicoding@gmail.com>
2023-08-28 22:44:42 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,24 @@ app.use(express.urlencoded({extended: true}));
 const studentRoute = require('./routes/studentRoute');
 app.use("/mahasiswa", studentRoute);
 
+app.post('/mahasiswa/addData', async (req, res) => {
+    try {
+        const { npm, nama, jurusan } = req.body
+        const book = new BookModel({
+            npm, nama, jurusan
+        })
+        const data = await book.save()
+        return res.status(200).json ({
+            msg: 'Ok',
+            data
+        })
+    } catch (error) {
+        return res.status(500).json({
+            msg: error.message
+        })
+    }
+})
+
 const PORT =process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
